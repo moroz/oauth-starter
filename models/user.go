800f7ba2512335d5cd062b30284e401f57830367
Karol Moroz <karol@moroz.dev>
2023-10-08 23:43:56 +0200

    Parse and format UUIDv7s

--- models/user.go
+++ models/user.go
@@ -3,11 +3,11 @@ package models
 import (
 	"time"
 
-	"github.com/GoWebProd/uuid7"
+	"github.com/moroz/oauth-starter/pkg/uuidv7"
 )
 
 type User struct {
-	Id         uuid7.UUID
+	Id         uuidv7.UUID
 	Email      string
 	InsertedAt time.Time `db:"inserted_at"`
 	UpdatedAt  time.Time `db:"updated_at"`
